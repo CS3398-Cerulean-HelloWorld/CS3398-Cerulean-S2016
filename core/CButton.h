d7d80e1163e267d79a0720f4a0176b153ac2946f
Zachary Westerman <zdw6@txstate.edu>
2016-10-06 01:13:04 -0500

    Changed CButton to CBoundsBox, as other page components will require a bounding box. CButton class now has a different purpose.

--- core/CButton.h
+++ core/CButton.h
@@ -1,102 +1,36 @@
 #pragma once
 
-#define getX(var) getX1(var)
-#define getY(var) getY1(var)
+#include "CBoundsBox.h"
 
 namespace CCore
 {
-	class CButton
+	class CButton : public CBoundsBox
 	{
-	private:
-		int X1;
-		int Y1;
-
-		int X2;
-		int Y2;
-
 	public:
-	    //Constructors
+		//Constructors
 		CButton();
 		CButton(int, int, int, int);
 
-		//function to set button position and size
-		void setBox(int, int, int, int);
-
-		//functions to directly set the button bounds
-		void setX1(int X_1) { X1 = X_1; }
-		void setY1(int Y_1) { Y1 = Y_1; }
-
-		void setX2(int X_2) { X2 = X_2; }
-		void setY2(int Y_2) { Y2 = Y_2; }
-
-		//functions to get the button bounds
-		int getX1() { return X1; }
-		int getY1() { return Y1; }
-
-		int getX2() { return X2; }
-		int getY2() { return Y2; }
-
-		//functions to set button position, maintaining button size.
-		void setX(int);
-		void setY(int);
-		void setPosition(int X, int Y) { setX(X); setY(Y); }
-
-		//functions to set button size
-		void setWidth(int Width) { X2 = X1 + Width; }
-		void setHeight(int Height) { Y2 = Y1 + Height; }
-		void setSize(int Width, int Height) { setWidth(Width); setHeight(Height); }
-
-		//functions to get button size
-		int getWidth() { return (X2 - X1); }
-		int getHeight() { return (Y2 - Y1); }
-
+		//function to return whether a point is within the button bounds
+		//(use this when determining if the user pressed a button)
+		bool pointIsInBounds(int, int);
 	};
 
 	//default constructor
-	CButton::CButton()
+	CButton::CButton() : CBoundsBox() {}
+
+	//constructor with full data
+	CButton::CButton(int X_pos, int Y_pos, int width, int height) :
+			 CBoundsBox(X_pos, Y_pos, width, height) {}
+
+	//function to return whether a point is within the button bounds
+	//(use this when determining if the user pressed a button)
+	bool CButton::pointIsInBounds(int X, int Y)
 	{
-		X1 = 0;
-		Y1 = 0;
-
-		X2 = 0;
-		Y2 = 0;
-	}
-
-	//full-data constructor
-	CButton::CButton(int X_pos, int Y_pos, int width, int height)
-	{
-		X1 = X_pos;
-		Y1 = Y_pos;
-
-		X2 = X_pos + width;
-		Y2 = Y_pos + height;
-	}
-
-	//function to set button position and size
-	void CButton::setBox(int X_pos, int Y_pos, int width, int height)
-	{
-		X1 = X_pos;
-		Y1 = Y_pos;
-
-		X2 = X_pos + width;
-		Y2 = Y_pos + height;
-	}
-
-	//function to set button X position, maintaining button width.
-	void CButton::setX(int X_pos)
-	{
-		int Width = X2 - X1;
-
-		X1 = X_pos;
-		X2 = X_pos + Width;
-	}
-
-	//function to set button Y position, maintaining button height.
-	void CButton::setY(int Y_pos)
-	{
-		int Height = Y2 - Y1;
-
-		Y1 = Y_pos;
-		Y2 = Y_pos + Height;
+		if ((X >= X1) && (X <= X2) &&
+			(Y >= Y1) && (Y <= Y2))
+			return true;
+		else
+			return false;
 	}
 }
