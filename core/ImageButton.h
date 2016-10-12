8ed6aa318ba1e97f86cc0cc36cb96018af16c5fa
Zachary Westerman <zdw6@txstate.edu>
2016-10-11 19:07:43 -0500

    Getting ImageButton class ready for Sprint 2

---
+++ core/ImageButton.h
@@ -0,0 +1,25 @@
+#ifndef IMAGEBUTTON_H_INCLUDED
+#define IMAGEBUTTON_H_INCLUDED
+
+#include "CTextButton.h"
+
+///TO DO: All of this.
+
+class ImageButton : public CTextButton
+{
+    /*private:
+		std::string BtnText;
+
+	public:
+		//Constructors
+		CTextButton();
+		CTextButton(const char*);
+		CTextButton(int, int, int, int);
+		CTextButton(int, int, int, int, const char*);
+
+		//functions to set and get the button text
+		void setText(const char*);
+		const char* getText();*/
+};
+
+#endif // IMAGEBUTTON_H_INCLUDED
